e06bc887a1618c38ec1c4b1eaf16cd1701004eee
Benubu <74571551+benubu@users.noreply.github.com>
2024-02-20 14:14:39 -0500

    Create hpdump.py

---
+++ Aliases and Snippets/hpdump.py
@@ -0,0 +1,42 @@
+!alias hpdump echo
+<drac2>
+c = combat
+newline = "\n"
+output = []
+
+#are we in combat?
+if not c:
+    desc.append("`!hpdump can only be used in combat.")
+
+monsters = []
+players = []
+others = []
+
+for combatant in combat().combatants:
+    if combatant.levels.get('Monster',None):
+        monsters.append(combatant)
+    elif combatant.levels.get('Monster',None) == None:
+        players.append(combatant)
+	else:
+	    others.append(combatant)
+		
+output = 'HP Dump\n'
+
+output += '---> Players <---\n'
+
+for player in players:
+    output += f'{player.name}: {player.hp_str()}\n'
+	
+output += '\n---> Monsters <---\n'
+
+for monster in monsters:
+    output += f'{monster.name}: {monster.hp_str()}\n'
+	
+if len(others):
+    output += '\n---> Others <---\n'
+
+for other in others:
+    output += f'{other.name}: {other.hp_str()}\n'
+
+return output
+</drac2>
